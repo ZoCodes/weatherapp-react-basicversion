f6cbfb036248e13f6165eeae5721565596a4bbf8
ZoCodes <zoeblogg91@gmail.com>
2021-10-12 23:23:33 +0100

    changed default city

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="New York" />
+        <Weather defaultCity="Leeds" />
 
         <footer>
           This project was coded by{" "}
