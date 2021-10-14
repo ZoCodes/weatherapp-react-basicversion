c2659bd265164745d42715e9903ba133ab24e385
ZoCodes <zoeblogg91@gmail.com>
2021-10-14 22:05:23 +0100

    made an asset folder for images

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -1,6 +1,6 @@
-// try to link media images
 import React from "react";
+import "./assets";
 
 export default function WeatherIcon(props) {
-  return <img src={props.code} alt="Weather" />;
+  return <img src={assets[props.code]} alt="Weather" />;
 }
